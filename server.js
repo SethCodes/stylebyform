38fa9182e2da15224d2c9fca2258c7b800d784df
Seth Stephens <sethastephens8@gmail.com>
2021-09-12 01:12:12 -0400

    push

--- server.js
+++ server.js
@@ -52,3 +52,7 @@ mongoose
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
