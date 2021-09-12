7e7394aa983568f2f5fb68ff3647ab1b1d53cd0b
Seth Stephens <sethastephens8@gmail.com>
2021-09-12 01:56:40 -0400

    push

--- server.js
+++ server.js
@@ -2,66 +2,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-require("dotenv").config();
 const app = express();
-const Blog = require("./models/Blog");
+const path = require("path");
 
+//access to .env variables
+require("dotenv").config();
+const MONGO_DB = process.env.ATLAS_URI;
 
+//routes
+const blogRoutes = require("./routes/Blogs");
+
+//middleware
 app.use(cors());
 app.use(express.json());
-
-const blogRoutes = require("./routes/Blogs");
 app.use("/blogs", blogRoutes);
 
-const uri = "mongodb+srv://KableAcademy:Kable@cluster4.9l4dq.mongodb.net/cinci-attractions?retryWrites=true&w=majority";
-const MONGO_DB = process.env.ATLAS_URI;
-console.log(MONGO_DB);
-
-
-mongoose
-  .connect(MONGO_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log("MongoDB has been connected"))
-  .catch((err) => console.log(err));
-
-  // app.get('/', (req, res) => {
-  //   Blog.find()
-  //   .then((blog) => {
-  //     const response = res.json(blog);
-  //     console.log(response);
-      
-
-  //   })
-  //   .catch(err => console.log(err));
-
-
-  // })
-
-// // Accessing the path module
-// const path = require("path");
-
-// // Step 1:
+// static files for heroku
 // app.use(express.static(path.resolve(__dirname, "./client/build")));
-// // Step 2:
 // app.get("*", function (request, response) {
 //   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 // });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
+//connect to db
+mongoose
+  .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
+
+//listen for port 5000 or prod
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
-});
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-// Accessing the path module
-const path = require("path");
-
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
