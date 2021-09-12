9079a23af6461b9789cd79bed2bf32805de50db1
Seth Stephens <sethastephens8@gmail.com>
2021-09-12 02:03:04 -0400

    boilerplate mern stack for heroku deploy

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ app.use(express.json());
 app.use("/blogs", blogRoutes);
 
 // static files for heroku
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
