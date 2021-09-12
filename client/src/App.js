eb2d1223807b14aa566c7747b24bc81235635930
Seth Stephens <sethastephens8@gmail.com>
2021-09-11 20:04:07 -0400

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import BACKEND_URL from "./config";
 
 
 function App() {
+
+  const [item, setItem] = useState("");
+
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(BACKEND_URL + "/blogs/" );
+      setItem(res.data);
+
+    };
+    getData();
+
+  }, []);
+
   return (
     <div className="App">
-     <h1>hello</h1>
+     {!item ? "loading..." : (
+       item.localeCompare((item)=> {
+         return(
+           <div className="">
+             <ul>
+               <li>{item.title}</li>
+             </ul>
+           </div>
+         )
+       })
+     )}
     </div>
   );
 }
