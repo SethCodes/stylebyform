c28f4688e8027fbc92ab997f7939ef549aa7d580
Seth Stephens <sethastephens8@gmail.com>
2021-09-30 10:42:42 -0400

    uri in config

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 const BACKEND_URL = "";
 
-// const BACKEND_URL = process.env.NODE_ENV === 'development'? "http://localhost:5000":"https://ancient-cliffs-40563.herokuapp.com/"
+// const BACKEND_URL = process.env.NODE_ENV === 'development'? "http://localhost:5000":"https://stylebyform.herokuapp.com/"
 
 export default BACKEND_URL;
