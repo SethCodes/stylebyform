e21b53600b3c9f16d972a5c317c861d91fc47964
Seth Stephens <sethastephens8@gmail.com>
2021-10-21 10:01:47 -0400

    update app.js and added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import BACKEND_URL from "./config";
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Menu from './components/Menu';
+import Photos from './components/Photos';
+import Reservation from './components/Reservation';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Success from './components/Success';
+import SignUp from './components/SignUp';
+
+
 
 
 function App() {
-
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const res = await axios.get(BACKEND_URL + "/blogs" );
-      setItems(res.data);
-    };
-    getData();
-  }, []);
-
   return (
+    <BrowserRouter>
+    <Navbar  />
     <div className="App">
-    <h1>hello</h1>
-    {
-      items.map((item) => {
-        return(
-          <div className="">
-            <ul>
-              <li>{item.title}</li>
-            </ul>
-          </div>
-        )
-      })
-    }
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/our-story" component={About} />
+      <Route exact path="/menu" component={Menu} />
+      <Route exact path="/photos" component={Photos} />
+      <Route exact path="/reservation" component={Reservation} />
+      <Route exact path="/login" component={Login} />
+      <Route path='/signup' component={SignUp} />
+      <Route path="/users/success" component={Success} />
+    </Switch>
+    <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
