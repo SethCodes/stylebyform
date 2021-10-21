ff16ee22d0b036cbf2069f8849ac313e439e98e3
Seth Stephens <sethastephens8@gmail.com>
2021-10-21 09:59:53 -0400

    new structure

---
+++ client/src/components/Reservation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Reservation = () => {
+
+<script src='https://square.site/appointments/buyer/widget/wcvp41wzuck37h/8MZMB2E1AZ34R.js'></script>
+
+
+
+
+    return (
+        <div>
+            <h1>Reservation Page</h1>
+        </div>
+    )
+}
+
+export default Reservation
