52702f0610feec28417c0eadd18883f28a1f0848
Seth Stephens <sethastephens8@gmail.com>
2021-10-21 10:57:11 -0400

    npm imports and home page workign

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import About from './components/About';
-import Menu from './components/Menu';
-import Photos from './components/Photos';
-import Reservation from './components/Reservation';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import Success from './components/Success';
-import SignUp from './components/SignUp';
+// import About from './components/About';
+// import Menu from './components/Menu';
+// import Photos from './components/Photos';
+// import Reservation from './components/Reservation';
+// import Footer from './components/Footer';
+// import Login from './components/Login';
+// import Success from './components/Success';
+// import SignUp from './components/SignUp';
 
 
 
@@ -21,15 +21,15 @@ function App() {
     <div className="App">
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/our-story" component={About} />
+      {/* <Route exact path="/our-story" component={About} />
       <Route exact path="/menu" component={Menu} />
       <Route exact path="/photos" component={Photos} />
       <Route exact path="/reservation" component={Reservation} />
       <Route exact path="/login" component={Login} />
       <Route path='/signup' component={SignUp} />
-      <Route path="/users/success" component={Success} />
+      <Route path="/users/success" component={Success} /> */}
     </Switch>
-    <Footer />
+    {/* <Footer /> */}
     </div>
     </BrowserRouter>
   );
